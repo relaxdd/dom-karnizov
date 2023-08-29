b94f0ec2220846254e7240b20da2a2bdcdcea10c
awenn2015 <44868028+relaxdd@users.noreply.github.com>
2023-08-29 20:04:11 +0400

    Новые страницы
    Добавил страницы "delivery", "payment", "price"

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,7 +1,17 @@
 import header from './includes/header'
 
 function main() {
-  header()
+  header();
+
+  (function prependBase() {
+    document.querySelectorAll('a').forEach((link) => {
+      let url = link.getAttribute('href')
+      if (url?.startsWith('/')) {
+        url = (import.meta.env?.BASE_URL || '/') + url.slice(1)
+        link.setAttribute('href', url)
+      }
+    })
+  })()
 }
 
 main()
--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,24 @@ import vitePugPlugin from "vite-plugin-pug-transformer";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: '/dom-karnizov/',
   appType: "mpa",
-  plugins: [vitePugPlugin({})],
+  plugins: [
+    vitePugPlugin({}),
+  ],
   build: {
     manifest: true,
     rollupOptions: {
       input: {
         index: resolve(__dirname, "index.html"),
-        order: resolve(__dirname, "order.html"),
+        checkout: resolve(__dirname, "checkout.html"),
         signin: resolve(__dirname, "signin.html"),
         signup: resolve(__dirname, "signup.html"),
         designer: resolve(__dirname, "designer.html"),
+        about: resolve(__dirname, "about.html"),
+        delivery: resolve(__dirname, "delivery.html"),
+        payment: resolve(__dirname, "payment.html"),
+        price: resolve(__dirname, "price.html"),
       },
     },
   },
