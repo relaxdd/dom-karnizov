9366e9b5fe133673b5cd93f7a9d29fdbf26bc7a9
awenn2015 <44868028+relaxdd@users.noreply.github.com>
2023-08-30 02:36:24 +0400

    Правки по структуре и конфигу

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,60 @@
-import { resolve } from "path";
+import { join, resolve } from "path";
 import vitePugPlugin from "vite-plugin-pug-transformer";
 import { defineConfig } from "vite";
+import * as glob from "glob";
+
+const indexDir = "/html/";
+
+const middleware = () => {
+  return {
+    name: "middleware",
+    apply: "serve",
+    configureServer(viteDevServer) {
+      return () => {
+        viteDevServer.middlewares.use(async (req, res, next) => {
+          const parts = req.originalUrl.split("/");
+
+          if (parts.at(-1) === "")
+            req.url = indexDir + req.originalUrl + "index.html";
+          else {
+            if (parts.at(-1).endsWith(".html"))
+              req.url = indexDir + req.url;
+            else {
+              res.writeHead(302, { "Location": req.originalUrl + "/" });
+              next();
+            }
+          }
+
+          // if (!req.originalUrl.endsWith(".html") && req.originalUrl !== "/")
+          //   req.url = indexDir + req.originalUrl + ".html";
+          // else if (req.url === "/index.html")
+          //   req.url = indexDir + req.url;
+
+          next();
+        });
+      };
+    },
+  };
+};
 
 export default defineConfig({
-  // base: '/dom-karnizov/',
-  appType: "mpa",
   plugins: [
+    middleware(),
     vitePugPlugin({}),
   ],
+  // base: '/dom-karnizov/',
+  // appType: "mpa",
+  root: join(__dirname, "src"),
+  publicDir: join(__dirname, "public"),
+  resolve: {
+    alias: { "/src": resolve(process.cwd(), "src") },
+  },
   build: {
+    outDir: join(__dirname, "dist"),
+    emptyOutDir: true,
     manifest: true,
     rollupOptions: {
-      input: {
-        index: resolve(__dirname, "index.html"),
-        checkout: resolve(__dirname, "checkout.html"),
-        signin: resolve(__dirname, "signin.html"),
-        signup: resolve(__dirname, "signup.html"),
-        designer: resolve(__dirname, "designer.html"),
-        about: resolve(__dirname, "about.html"),
-        delivery: resolve(__dirname, "delivery.html"),
-        payment: resolve(__dirname, "payment.html"),
-        price: resolve(__dirname, "price.html"),
-        services: resolve(__dirname, "services.html"),
-        "our-works": resolve(__dirname, "our-works.html"),
-      },
+      input: glob.sync("./src/html/**/*.html"),
     },
   },
 });
