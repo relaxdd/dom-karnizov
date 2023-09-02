e3d3df2ea32dd3a117d5fa0d039a1232cd1cb1c0
awenn2015 <44868028+relaxdd@users.noreply.github.com>
2023-09-03 03:20:05 +0400

    Новый костыль в скрипты для gh-pages

--- src/ts/includes/utils.ts
+++ src/ts/includes/utils.ts
@@ -1,9 +1,18 @@
 export function prependBase() {
+  const base = import.meta.env?.BASE_URL || '/'
+  if (base === '/') return
+
   document.querySelectorAll('a').forEach((link) => {
-    let url = link.getAttribute('href')
-    if (url?.startsWith('/')) {
-      url = (import.meta.env?.BASE_URL || '/') + url.slice(1)
-      link.setAttribute('href', url)
+    const href = link.getAttribute('href')
+
+    if (href?.startsWith('/')) {
+      link.setAttribute('href', base + href.slice(1))
     }
+  })
+
+  document.querySelectorAll('img').forEach((img) => {
+    const src = img.getAttribute('src')
+    if (!src || src?.startsWith(base)) return
+    img.setAttribute('src', base + src.slice(1))
   })
 }
